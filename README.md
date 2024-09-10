0ca3c8d47b105245081aa9154afc0cfeb1cf34f7
Kazumasa Utashiro <kaz@utashiro.com>
2024-09-10 19:23:06 +0900

    implement local option parsing

--- README.md
+++ README.md
@@ -5,9 +5,49 @@ glob - optex filter to glob filenames
 
 # SYNOPSIS
 
-optex -Mglob pattern -- command
+optex -Mglob \[ option \] pattern -- command
 
 # DESCRIPTION
+
+This module is used to select filenames given as arguments by pattern.
+
+For example, the following will pass only files matching `*.c` from
+`*/*` as arguments to `ls`.
+
+    optex -Mglob '*.c' -- ls */*
+
+There are several unique options that are valid only for this module.
+
+- **--exclude**
+
+    Option `--exclude` will mean the opposite.
+
+        optex -Mglob --exclude '*.c' -- ls */*
+
+- **--regex**
+
+    If the `--regex` option is given, it is evaluated as a regular
+    expression instead of a glob pattern.
+
+        optex -Mglob --regex '\.c$' -- ls */*
+
+- **--path**
+
+    With the `--path` option it matches against the entire path, not just
+    the filename.
+
+        optex -Mglob --path '\.c$' -- ls */*
+
+Multiple options can be specified at the same time.
+
+    optex -Mglob --exclude --regex --path '^/.*\.c$' -- ls */*
+
+# CONSIDERATION
+
+You should also consider using the **extglob** feature of [bash(1)](http://man.he.net/man1/bash).
+
+For example, you can use `!(*.EN).md` would specify files matching
+`*.md` minus those matching `*.EN.md`.
 
 # AUTHOR
 
--- lib/App/optex/glob.pm
+++ lib/App/optex/glob.pm
@@ -19,21 +19,32 @@ my %opt = (
 );
 lock_keys %opt;
 
-sub initialize {
-    our($mod, $argv) = @_;
+use List::Util qw(pairmap);
+
+sub hash_to_spec {
+    pairmap {
+	if    (not defined $b) { "$a"   }
+	elsif ($b =~ /^\d+$/)  { "$a=i" }
+	else                   { "$a=s" }
+    } %{+shift};
 }
 
 sub finalize {
-    our($mod, $argv);
-    my $i = first { $argv->[$_] eq '--' } keys @$argv;
-    $i // return;
+    my($mod, $argv) = @_;
+    my $i = (first { $argv->[$_] eq '--' } keys @$argv) // return;
     splice @$argv, $i, 1; # remove '--'
     my @glob = splice @$argv, 0, $i or return;
+
+    use Getopt::Long qw(GetOptionsFromArray);
+    Getopt::Long::Configure qw(bundling require_order);
+    GetOptionsFromArray \@glob, \%opt, hash_to_spec \%opt
+	or die "Option parse error.\n";
+
     my @glob_re = do {
 	if ($opt{regex}) {
-	    map { qr/$_/ } @glob;
+	    map qr/$_/, @glob;
 	} else {
-	    map { qr/${ glob_to_regex($_) }/ } @glob;
+	    map qr/${ glob_to_regex($_) }/, @glob;
 	}
     };
     my($match, $unmatch) = $opt{exclude} ? (0, 1) : (1, 0);
@@ -50,10 +61,10 @@ sub finalize {
 }
 
 sub set {
-    while (my($k, $v) = splice @_, 0, 2) {
-	$opt{$k} = $v;
-    }
+    pairmap { $opt{$a} = $b } @_;
 }
+
+1;
 
 =encoding utf-8
 
@@ -63,9 +74,53 @@ glob - optex filter to glob filenames
 
 =head1 SYNOPSIS
 
-optex -Mglob pattern -- command
+optex -Mglob [ option ] pattern -- command
 
 =head1 DESCRIPTION
+
+This module is used to select filenames given as arguments by pattern.
+
+For example, the following will pass only files matching C<*.c> from
+C<*/*> as arguments to C<ls>.
+
+    optex -Mglob '*.c' -- ls */*
+
+There are several unique options that are valid only for this module.
+
+=over 7
+
+=item B<--exclude>
+
+Option C<--exclude> will mean the opposite.
+
+    optex -Mglob --exclude '*.c' -- ls */*
+
+=item B<--regex>
+
+If the C<--regex> option is given, it is evaluated as a regular
+expression instead of a glob pattern.
+
+    optex -Mglob --regex '\.c$' -- ls */*
+
+=item B<--path>
+
+With the C<--path> option it matches against the entire path, not just
+the filename.
+
+    optex -Mglob --path '\.c$' -- ls */*
+
+=back
+
+Multiple options can be specified at the same time.
+
+    optex -Mglob --exclude --regex --path '^/.*\.c$' -- ls */*
+
+=head1 CONSIDERATION
+
+You should also consider using the B<extglob> feature of L<bash(1)>.
+
+For example, you can use C<!(*.EN).md> would specify files matching
+C<*.md> minus those matching C<*.EN.md>.
 
 =head1 AUTHOR
 
@@ -79,7 +134,5 @@ This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself.
 
 =cut
-
-1;
 
 __DATA__
